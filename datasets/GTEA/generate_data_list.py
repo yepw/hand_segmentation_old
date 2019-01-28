f0ff0d6386296b4361182d30d9cf546e8dc424be
intuitivecomputing <yepingw@outlook.com>
2019-01-28 18:35:42 -0500

    add png image

--- datasets/GTEA/generate_data_list.py
+++ datasets/GTEA/generate_data_list.py
@@ -1,5 +1,6 @@
 import os
 import random
+from PIL import Image
 
 root = "/home/intuitivecomputing/light-weight-refinenet/datasets/GTEA/Images"
 
@@ -9,12 +10,14 @@ f2 = open('val.gtea', 'w')
 for path, subdirs, files in os.walk(root):
     for name in files:
         print name
+	im = Image.open('Images/'+name)
+	name = name[0:len(name)-3]+'png'
+	im.save('Images/'+name)
 	if random.random()<0.8:
-	    mask_name = name[0:len(name)-3]+'png'
-            f1.write('Images/'+name+'\t'+'Masks/'+mask_name+'\n')
+            f1.write('Images/'+name+'\t'+'Masks/'+name+'\n')
 	else:
-  	    mask_name = name[0:len(name)-3]+'png'
-            f2.write('Images/'+name+'\t'+'Masks/'+mask_name+'\n')
+            f2.write('Images/'+name+'\t'+'Masks/'+name+'\n')
+	
 f1.close()
 f2.close()
 	    
--- src/datasets.py
+++ src/datasets.py
@@ -199,7 +199,9 @@ class NYUDataset(Dataset):
                 img_arr = np.tile(img_arr, [3, 1, 1]).transpose(1, 2, 0)
             return img_arr
         image = read_image(img_name)
+	print(image)
         mask = np.array(Image.open(msk_name))
+	print(mask)
         if img_name != msk_name:
             assert len(mask.shape) == 2, 'Masks must be encoded without colourmap'
         sample = {'image': image, 'mask': mask}
@@ -242,15 +244,15 @@ class GTEADataset(Dataset):
         img_name = os.path.join(self.root_dir, self.datalist[idx][0])
         msk_name = os.path.join(self.root_dir, self.datalist[idx][1])
         def read_image(x):
-            img_arr = np.array(Image.open(x).resize((625,468)))
+            img_arr = np.array(Image.open(x).resize((468,625)))
             if len(img_arr.shape) == 2: # grayscale
 		print('grayscale')
                 img_arr = np.tile(img_arr, [3, 1, 1]).transpose(1, 2, 0) 
             return img_arr
         image = read_image(img_name)
-	print(image)
-        mask = 255 - np.array(Image.open(msk_name).resize((625,468)))
-        print(mask)
+	# print(image)
+        mask = np.array(Image.open(msk_name).resize((468,625)))
+        # print(mask)
         if img_name != msk_name:
             assert len(mask.shape) == 2, 'Masks must be encoded without colourmap'
         sample = {'image': image, 'mask': mask}
