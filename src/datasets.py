9512c5e99f956220e1538a1e60a363918f47b27a
intuitivecomputing <yepingw@outlook.com>
2019-01-28 17:13:22 -0500

    255-

--- src/datasets.py
+++ src/datasets.py
@@ -248,7 +248,9 @@ class GTEADataset(Dataset):
                 img_arr = np.tile(img_arr, [3, 1, 1]).transpose(1, 2, 0) 
             return img_arr
         image = read_image(img_name)
-        mask = np.array(Image.open(msk_name).resize((625,468)).convert("P"))
+	print(image)
+        mask = 255 - np.array(Image.open(msk_name).resize((625,468)))
+        print(mask)
         if img_name != msk_name:
             assert len(mask.shape) == 2, 'Masks must be encoded without colourmap'
         sample = {'image': image, 'mask': mask}
